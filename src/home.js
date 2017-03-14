98c544b1f66671d9aea4d500d8c4c1cb0e95a900
ttjkst <791599901@qq.com>
2017-03-14 23:26:12 +0800

    ss

--- src/home.js
+++ src/home.js
@@ -5,35 +5,27 @@ import {centers as center} from './controller.js';
 import './foo.js';
 import './homeAction.js';
 class  LoginFrom extends React.Component{
-	
-}
-class Modal extends React.Component{
 	constructor(props){
 		super(props);
 		this.state={
-			isShow:true,
-			username:"ssss",
-			password:"mm"
+			username:"",
+			password:""
 		}
 	}
 	componentDidMount(){
-		//center.resigter("modal",this);
+		center.resigter("LoginFrom",this);
 	}
 	componentDidUnMount(){
-		//center.remove("modal");
+		center.remove("LoginFrom");
 	}
 	changeUsername(e){
-		console.info(this);
-		//center.dispatch("modal","changeUsername",e.target.value);
+		center.dispatch("LoginFrom","changeUsername",e.target.value);
 	}
 	changePassword(e){
-		//center.dispatch("modal","changePassword",e);
+		center.dispatch("LoginFrom","changePassword",e.target.value);
 	}
 	render(){
-		return <ModalWithCloseButton show={this.state.isShow?'show':'hide'}>
-					<ModalHeader>Test Modal</ModalHeader>
-					<ModalBody>
-						  <form>
+				return 		  <form>
 								    <div className="form-group">
 									      	<label  className="control-label"  >用户名：</label>
 									        <input  type="text" value={this.state.username} placeholder="请输入用户名"  onChange={this.changeUsername} className="form-control"/>
@@ -43,7 +35,27 @@ class Modal extends React.Component{
 									       	<input  type="password" value={this.state.password} placeholder="请输入密码" onChange={this.changePassword}  className="form-control" />
 								     	</div>
 								     	<button type="button" className="btn btn-primary" onClick={this.hindleLogin} >登入</button>
-							     </form>  	
+							     </form>
+		}
+}
+class Modal extends React.Component{
+	constructor(props){
+		super(props);
+		this.state={
+			isShow:true,
+		}
+	}
+	componentDidMount(){
+		center.resigter("modal",this);
+	}
+	componentDidUnMount(){
+		center.remove("modal");
+	}
+	render(){
+		return <ModalWithCloseButton show={this.state.isShow?'show':'hide'}>
+					<ModalHeader>Test Modal</ModalHeader>
+					<ModalBody>
+							<LoginFrom/>
 					</ModalBody>
 			</ModalWithCloseButton>
 	}
--- src/homeAction.js
+++ src/homeAction.js
@@ -1,17 +1,19 @@
 import {factory} from './controller.js'
-var modalController = factory.createOne("modal");
+let modalController = factory.createOne("modal");
 modalController.register("init",(_this,center)=>{
 	_this.setState({
 		isShow:true
 	})
 })
-modalController.register("changeUsername",(_this,center,rest)=>{
+let  loginfromController = factory.createOne("LoginFrom");
+loginfromController.register("changeUsername",(_this,center,rest)=>{
 	console.info(_this);
 	_this.setState({
-		"username":rest
+		username:rest
 	})
 })
-modalController.register("changePassword",(_this,center,rest)=>{
+loginfromController.register("changePassword",(_this,center,rest)=>{
+	console.info(_this);
 	_this.setState({
 		"password":rest
 	})
